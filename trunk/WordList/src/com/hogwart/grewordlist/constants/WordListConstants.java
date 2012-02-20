4aba5cdcd7c9208e8e4e15d991656b73ac314ea1
leo.shabr@gmail.com <leo.shabr@gmail.com@a34d5315-8206-2388-6452-20bacf9ae45d>
2012-02-20 08:50:49 +0000

    separate list

---
+++ trunk/WordList/src/com/hogwart/grewordlist/constants/WordListConstants.java
@@ -0,0 +1,7 @@
+package com.hogwart.grewordlist.constants;
+
+public interface WordListConstants {
+
+	public static final String LIST_INDEX = "list_index";
+	public static final String LAST_LIST_INDICATOR = "lastListIndicator";
+}
