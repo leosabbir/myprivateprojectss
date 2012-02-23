a5fb255d75915fb9e3fe94c26203b32f43c86d1e
leo.shabr@gmail.com <leo.shabr@gmail.com@a34d5315-8206-2388-6452-20bacf9ae45d>
2012-02-23 16:40:54 +0000

    comet listener

---
+++ trunk/crackTheCode/src/com/hogwart/crackthecode/client/listener/CrackCodeCometListener.java
@@ -0,0 +1,46 @@
+package com.hogwart.crackthecode.client.listener;
+
+import java.io.Serializable;
+import java.util.List;
+
+import net.zschech.gwt.comet.client.CometListener;
+
+public class CrackCodeCometListener implements CometListener{
+
+	@Override
+	public void onConnected(int heartbeat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onError(Throwable exception, boolean connected) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onHeartbeat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRefresh() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMessage(List<? extends Serializable> messages) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
