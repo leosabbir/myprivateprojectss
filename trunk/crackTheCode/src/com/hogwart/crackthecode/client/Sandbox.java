09fde61aeefb7b34117d7460925452abaa64476c
leo.shabr@gmail.com <leo.shabr@gmail.com@a34d5315-8206-2388-6452-20bacf9ae45d>
2012-02-24 09:00:03 +0000

    http session

--- trunk/crackTheCode/src/com/hogwart/crackthecode/client/Sandbox.java
+++ trunk/crackTheCode/src/com/hogwart/crackthecode/client/Sandbox.java
@@ -1,10 +1,44 @@
 package com.hogwart.crackthecode.client;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Sandbox {
 	public static void main(String[] args) {
 		for(int i=0; i < 100; i++){
 			System.out.print((int) (Math.random() * 8)+1);
 			System.out.print(" ");
 		}
+		
+		try {
+			System.out.println(compareDate("05-April-86", "05-April-87"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	/*
+	 * return "0" when both dates are exactly equal
+	 * return "1" when firstDate is greater than secondDate and
+	 * return "-1" when firstDate is less than secondDate.
+	 * 
+	 */
+	public static int compareDate(String dateStr1, String dateStr2) throws ParseException{
+		
+		DateFormat formatter ; 
+		Date date1;
+		Date date2;
+		formatter = new SimpleDateFormat("dd-MMM-yy");
+		
+		date1 = (Date) formatter.parse(dateStr1);
+		date2 = (Date) formatter.parse(dateStr2);
+		
+		
+		return date1.compareTo(date2);		
 	}
 }
--- trunk/crackTheCode/src/com/hogwart/crackthecode/server/service/ColorCodeServiceImpl.java
+++ trunk/crackTheCode/src/com/hogwart/crackthecode/server/service/ColorCodeServiceImpl.java
@@ -3,6 +3,9 @@ package com.hogwart.crackthecode.server.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.hogwart.crackthecode.shared.ColorCode;
 import com.hogwart.crackthecode.shared.api.ColorCodeService;
@@ -15,9 +18,16 @@ public class ColorCodeServiceImpl extends RemoteServiceServlet implements ColorC
 	private static final long serialVersionUID = 1L;
 	
 	ColorCode colorCode;
+	
+	public ColorCodeServiceImpl() {
+		super();
+		
+		
+	}
 
 	@Override
 	public void createColorCode() {
+		
 		List<Integer> codes = new ArrayList<Integer>();
 		
 		while( codes.size() < 5 ){
@@ -29,6 +39,7 @@ public class ColorCodeServiceImpl extends RemoteServiceServlet implements ColorC
 		}
 		
 		colorCode = new ColorCode(codes.get(0), codes.get(1), codes.get(2), codes.get(3), codes.get(4));
+		getSession().setAttribute(getSession().getId(), colorCode);
 		
 	}
 
@@ -43,8 +54,13 @@ public class ColorCodeServiceImpl extends RemoteServiceServlet implements ColorC
 			
 	@Override
 	public List<Integer> matchColorCode(ColorCode code) {
-		System.out.println(this.colorCode.toString());
-		return this.colorCode.getMatches(code);
+		ColorCode orginalColorCode = (ColorCode) getSession().getAttribute(getSession().getId());
+		System.out.println(orginalColorCode.toString());
+		return orginalColorCode.getMatches(code);
+	}
+	
+	private HttpSession getSession(){
+		return this.getThreadLocalRequest().getSession();
 	}
 
 }
